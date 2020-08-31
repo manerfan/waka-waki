a1f9ff6740906de52244b99670b2aebc30967253
yongyong.fyy <yongyong.fyy@alibaba-inc.com>
2020-08-31 23:23:47 +0800

    1. docker-mysql 2. docker-nacos-cluster 3. spring-cloud-gateway (not completed)

---
+++ waka-waki-gateway/src/main/java/com/manerfan/waka/waki/gateway/WakaWakiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.manerfan.waka.waki.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakaWakiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakaWakiGatewayApplication.class, args);
+	}
+
+}
---
+++ waka-waki-gateway/src/test/java/com/manerfan/waka/waki/gateway/WakaWakiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manerfan.waka.waki.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakaWakiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
