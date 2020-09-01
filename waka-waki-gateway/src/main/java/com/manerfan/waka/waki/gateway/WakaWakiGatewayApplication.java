c4281138700829c867460d8faa1f58a77f7e2819
yongyong.fyy <yongyong.fyy@alibaba-inc.com>
2020-09-02 00:30:04 +0800

    add nginx for dns & cdn

--- waka-waki-gateway/src/main/java/com/manerfan/waka/waki/gateway/WakaWakiGatewayApplication.java
+++ waka-waki-gateway/src/main/java/com/manerfan/waka/waki/gateway/WakaWakiGatewayApplication.java
@@ -2,12 +2,18 @@ package com.manerfan.waka.waki.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Waka Waki Gateway
+ *
+ * @author manerfan
+ * @date 2020-09-01
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WakaWakiGatewayApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WakaWakiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WakaWakiGatewayApplication.class, args);
+    }
 }
