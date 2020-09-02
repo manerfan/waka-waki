7e9f5a5ca4de1de3dadfb82e13a13c9075066ca1
yongyong.fyy <yongyong.fyy@alibaba-inc.com>
2020-09-02 21:16:42 +0800

    add general service-dockerfile

---
+++ deploy/waka-waki/deployment/waka-waki-services/docker-startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+JAVA_OPT="-server -Xms${JVM_XMS} -Xmx${JVM_XMX} -Xmn${JVM_XMN}"
+JAVA_OPT="${JAVA_OPT} -jar /opt/application.jar"
+JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
+
+mkdir -p /var/logs/${SERVICE_NAME}
+
+echo "${SERVICE_NAME} is starting, you can check the /var/logs/${SERVICE_NAME}/start.out"
+echo "java ${JAVA_OPT}" > /var/logs/${SERVICE_NAME}/start.out 2>&1 &
+nohup java ${JAVA_OPT} > /var/logs/${SERVICE_NAME}/start.out 2>&1 < /dev/null
